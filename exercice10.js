1f4442fff4443190396cf74650167afee00889c3
Yanting Li <yali@c129-10.enst.fr>
2017-01-13 16:59:32 +0100

    ---

--- exercice10.js
+++ exercice10.js
@@ -15,20 +15,14 @@ function loadDoc2(){
     var div = document.getElementById("div2");
     var text = this.responseText;
     var lines = text.split("\n");
+    var colorInit = "#DCDCDF";
     for(var i = 0; i < lines.length; i++){
       var p = document.createElement("p");
+      p.setAttribute("style", "color:"+colorInit";");
+      /*p.style.color*/
       p.textContent = lines[i];
       div.appendChild(p);
     }
   }
   xhr.send();
-
-  /*var lines = text.split('\n');
-  for(var i = 0; i < lines.length; i++){
-    var p = document.createElement("p");
-    p.textContent = lines[i];
-    div.appendChild(p);
-  }*/
-/*}
-xhr.send();*/
 }
