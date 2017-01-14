3f012c5c787d0459150cc80ae042918f3a158ed4
Yanting Li <yali@c128-03.enst.fr>
2017-01-14 21:26:58 +0100

    Committed at 21:25 14-01-2017 a little slow. But as I have said, every thing will go well.

---
+++ chat.php
@@ -0,0 +1,15 @@
+<?php
+
+// cr�ation de la chaine � ajouter dans le fichier
+$chaine = "<br />- ";
+$chaine .= "<a href='javascript:recherche(\"" . gethostbyname($_SERVER['SERVER_NAME']) . "\");'>" . gethostbyname($_SERVER['SERVER_NAME']) . "</a>";
+$chaine .=  " - " . $_GET['phrase'];
+
+
+$fp = fopen("texte.html","a");
+
+fwrite($fp, $chaine);
+fclose($fp);
+echo "Ecriture reussie";
+
+?>
--- exercice1.js
+++ exercice1.js
@@ -3,25 +3,34 @@ function loadDoc(){
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "text.txt");
   xhr.onload = function(){
-    document.getElementById("textarea").textContent = this.responseText;
-  }
+    document.getElementById("textarea").textContent = xhr.responseText;
+  };
   xhr.send();
 }
 
 function loadDoc2(){
   var xhr = new XMLHttpRequest();
   xhr.open("GET", "text.txt");
-  xhr.onload = function(){
-    /*var body = getElementsByTagName("body")[0];*/
+  xhr.onload = function() {
     var div = document.getElementById("div2");
-    var text = this.responseText;
-    div.appendChild(text);
-    /*var lines = text.split('\n');
+    var text = xhr.responseText;
+    var lines = text.split("\n");
+
+    var colorInitHex = 0x3399ff;                  //Set a initial color, type:hex
     for(var i = 0; i < lines.length; i++){
       var p = document.createElement("p");
+      var colorNewHex = colorInitHex + 0x20 * i;  //Create a new color from the initial color
+      var colorStr = colorNewHex.toString(16);    //Parse the new color (hex type) into string type
+      /* Problem!!!!!
+      var colorNewHex = 0x34;
+      colorNewHex.toString(16);                 //52
+      var colorStr = colorNewHex.toString(16);  //34
+      ?
+      */
+      p.style.color = "#" + colorStr;
       p.textContent = lines[i];
       div.appendChild(p);
-    }*/
-  }
+    }
+  };
   xhr.send();
 }
